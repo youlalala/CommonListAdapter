2251d13fde7f6d8b3e5559bf2a189a2d8fe8f363
ulalala <reyoula@gmail.com>
2022-07-14 19:49:17 +0900

    json file add

--- app/src/main/java/com/ulalala/commonlistadapter/MainActivity.kt
+++ app/src/main/java/com/ulalala/commonlistadapter/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ulalala.commonlistadapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val json = assets.open("a.json").reader().readText()
+        Log.d("youla",json)
     }
 }
